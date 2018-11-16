f97c06a304ed531197fa59608155296456a91bc2
Naruhiko Nishino <naru123456789@gmail.com>
2018-11-16 20:01:11 +0900

    Update appveyor.bat

--- src/appveyor.bat
+++ src/appveyor.bat
@@ -8,8 +8,13 @@ set INCLUDE=%INCLUDE%C:\Program Files (x86)\Windows Kits\8.1\Include\um
 cd %APPVEYOR_BUILD_FOLDER%
 cd src
     sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
+if "%FEATURE%" == "HUGE" (
     nmake -f Make_mvc2.mak DIRECTX=yes CPU=i386 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35 FEATURES=%FEATURE% || exit 1
     nmake -f Make_mvc2.mak DIRECTX=yes CPU=i386 CHANNEL=yes OLE=no GUI=no  IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35 FEATURES=%FEATURE% || exit 1
+) ELSE (
+    nmake -f Make_mvc2.mak CPU=i386 OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+    nmake -f Make_mvc2.mak CPU=i386 OLE=no GUI=no IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+) 
     move /Y .\gvim.exe .\gvim-x86.exe
     move /Y .\vim.exe .\vim-x86.exe
 cd ..
@@ -20,8 +25,13 @@ set INCLUDE=%INCLUDE%C:\Program Files (x86)\Windows Kits\8.1\Include\um
 cd %APPVEYOR_BUILD_FOLDER%
 cd src
     sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
+ if "%FEATURE%" == "HUGE" (
     nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
     nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=no  IME=yes MBYTE=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
+) ELSE (
+    nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=yes IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+    nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=no IME=yes MBYTE=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+) 
     move /Y .\gvim.exe .\gvim-x64.exe
     move /Y .\vim.exe .\vim-x64.exe
 cd ..
