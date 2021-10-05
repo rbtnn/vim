18c72d5e3097a48fa5d58ac5ef4b3948098c5933
rbtnn <naru123456789@gmail.com>
2021-10-05 17:05:21 +0900

    fix screenpos for tabsidebar

--- src/move.c
+++ src/move.c
@@ -1308,7 +1308,11 @@ f_screenpos(typval_T *argvars UNUSED, typval_T *rettv)
     textpos2screenpos(wp, &pos, &row, &scol, &ccol, &ecol);
 
     dict_add_number(dict, "row", row);
-    dict_add_number(dict, "col", scol);
+    dict_add_number(dict, "col", scol
+#if defined(FEAT_TABSIDEBAR)
+	    + tabsidebar_leftcol(wp)
+#endif
+	    );
     dict_add_number(dict, "curscol", ccol);
     dict_add_number(dict, "endcol", ecol);
 }
